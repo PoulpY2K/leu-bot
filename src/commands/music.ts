511f2d9081f12358b2dd327e67e0b15af4892a27
PoulpY2K <poulpy2k@gmail.com>
2022-04-22 18:56:01 +0200

    build(*): initial commit of the project

---
+++ src/commands/music.ts
@@ -0,0 +1,22 @@
+import { Player, Queue } from "@discordx/music";
+import type {
+  Guild,
+  TextBasedChannel,
+} from "discord.js";
+import { Message } from 'discord.js';
+
+export class MyQueue extends Queue {
+  lastControlMessage?: Message;
+  timeoutTimer?: NodeJS.Timeout;
+  lockUpdate = false;
+
+  constructor(player: Player, guild: Guild, public channel?: TextBasedChannel) {
+    super(player, guild);
+  }
+}
+
+export class MyPlayer extends Player {
+  constructor() {
+    super();
+  }
+}
